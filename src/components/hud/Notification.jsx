0de17980ed2feee2f58e3d74db4846fa1dd62582
MarshLalor <marshl13s@gmail.com>
2026-04-27 11:37:04 -0400

    Add files via upload
    updates

--- src/components/hud/Notification.jsx
+++ src/components/hud/Notification.jsx
@@ -1,11 +1,14 @@
-import useGameStore from '../../store/gameStore'
+import useGameStore from '../../store/gameStore';
 
 export default function Notification() {
-  const notification = useGameStore(s => s.notification)
+  const notification = useGameStore(s => s.notification);
+
+  // Don't render anything when there's no message
+  if (!notification) return null;
 
   return (
     <div className={`notification ${notification ? 'show' : ''}`}>
       {notification}
     </div>
-  )
+  );
 }
